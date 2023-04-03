9b79da629f5b4807eda3869e64a457b32b265073
Hanjae Lee <jayhanjaelee@gmail.com>
2023-04-03 21:02:29 +0900

    chore: add allowed hosts.

--- payhere/settings.py
+++ payhere/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hanjaelee.com", "www.hanjaelee.com"]
 
 
 # Application definition
